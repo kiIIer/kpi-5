08cfaae1fcef1d7b2f81bf66ddccd246551b0c51
killer <michael.molchanov.2004@gmail.com>
2023-10-09 15:20:13 +0200

    deleted copy, ill work in original instead

--- cpp/lab/lab-2-09.10.2023/workermanager/commands/Add/IAddCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "IAddCommand.h"
-#include "../../tools/IJsonParser.h"
-#include "AddCommand.h"
-
-fruit::Component<IAddCommand> getIAddCommandComponent() {
-    return fruit::createComponent().install(getIJsonParserComponent).bind<IAddCommand, AddCommand>();
-}
-
--- cpp/lab/lab-2-09.10.2023/workermanager/tools/IJsonParser.cpp
+++
@@ -1,7 +0,0 @@
-#include "fruit/fruit.h"
-#include "IJsonParser.h"
-#include "JsonParser.h"
-
-fruit::Component <IJsonParser> getIJsonParserComponent() {
-    return fruit::createComponent().bind<IJsonParser, JsonParser>();
-}
