ef2f97abd06c4384c2414543031c8403eefe9b36
killer <michael.molchanov.2004@gmail.com>
2023-10-18 12:50:14 +0200

    added other fnc

--- ai/lab/lab-3-17.10.2023/fuzzyclust/main.py
+++ ai/lab/lab-3-17.10.2023/fuzzyclust/main.py
@@ -10,6 +10,7 @@ X[:100, :] = np.random.normal(loc=(-5, -5), scale=1, size=(100, 2))
 X[100:200, :] = np.random.normal(loc=(0, 5), scale=1, size=(100, 2))
 X[200:300, :] = np.random.normal(loc=(5, -5), scale=1, size=(100, 2))
 
+# Original plot of data points before clustering
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.scatter(X[:, 0], X[:, 1], color='grey', marker='o')
@@ -23,7 +24,7 @@ cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
 # Find the cluster membership of each data point
 cluster_membership = np.argmax(u, axis=0)
 
-# 4. Plot the data after clustering
+# Plot the data after clustering
 plt.subplot(1, 2, 2)
 for i in range(n_clusters):
     plt.scatter(X[cluster_membership == i, 0], X[cluster_membership == i, 1], label=f'Cluster {i + 1}')
@@ -32,4 +33,14 @@ plt.scatter(cntr[:, 0], cntr[:, 1], s=200, c='black', marker='X', label='Centroi
 plt.title("After Fuzzy C-Means Clustering")
 plt.legend()
 plt.tight_layout()
+plt.show()
+
+# Plot the objective function values over iterations
+plt.figure(figsize=(8, 5))
+plt.plot(jm, 'o-', linewidth=2)
+plt.title("Objective Function Values Over Iterations")
+plt.xlabel("Iterations")
+plt.ylabel("Objective Function Value")
+plt.tight_layout()
+plt.grid(True)
 plt.show()
