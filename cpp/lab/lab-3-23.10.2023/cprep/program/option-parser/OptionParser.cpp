6daecd27881da8169c52b6a9cf338e362b5ec253
kiIIer <39669501+kiIIer@users.noreply.github.com>
2023-10-25 18:59:29 +0300

    seq in lab 3 cli (#31)

--- cpp/lab/lab-3-23.10.2023/cprep/program/option-parser/OptionParser.cpp
+++ cpp/lab/lab-3-23.10.2023/cprep/program/option-parser/OptionParser.cpp
@@ -9,23 +9,20 @@ std::unique_ptr<UserOptions> OptionParser::parse(CLI::App &app) {
 
     app.add_flag("-i,--ignore-case", options->ignoreCase, "Perform a case insensitive search.");
 
-    // Invert match
     app.add_flag("-v,--invert-match", options->invertMatch, "Invert the sense of matching.");
 
-    // Regex pattern (e.g., for -e option)
     app.add_option("-e,--regex", options->patterns, "Specify the regex pattern.")
             ->required();
 
-    // File to search
     app.add_option("-f,--file", options->inputFile, "File to search.");
 
-    // Line number
     app.add_flag("-n,--line-number", options->showLineNumber, "Print line number with output.");
 
-    // Number of threads
     app.add_option("--threads", options->threads, "Number of worker threads.");
 
     app.add_option("-l,--log", options->log_file, "Log file path (default: /dev/null)");
+
+    app.add_flag("-s,--sequenced", options->unorderedOut, "Display results in sequence, if unset, display as soon as a match is found.");
 
     return options;
 }
