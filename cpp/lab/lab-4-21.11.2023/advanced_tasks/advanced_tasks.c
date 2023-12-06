69e4e2793614531e80e4f08b8decf905fd4fb05d
killer <michael.molchanov.2004@gmail.com>
2023-12-06 05:41:35 +0100

    formated files and prepared report

--- cpp/lab/lab-4-21.11.2023/advanced_tasks/advanced_tasks.c
+++ cpp/lab/lab-4-21.11.2023/advanced_tasks/advanced_tasks.c
@@ -10,24 +10,26 @@ MODULE_AUTHOR("Mike Molchanov");
 MODULE_DESCRIPTION("An advanced Linux colonel module to complete tasks 1-7");
 MODULE_VERSION("0.2");
 
-struct my_list {
+struct my_list
+{
     int data;
     struct list_head list;
 };
 
 static LIST_HEAD(my_list_head);
 
-static int __init advanced_kernel_init(void) {
+static int __init advanced_kernel_init(void)
+{
     struct my_list *new_node;
     struct my_list *cursor;
-    int test_data[] = {12, 16, 26, 9, 12, 12, 10}; // Example data
+    int test_data[] = {12, 16, 26, 9, 12, 12, 10};
     int i;
     int conjunction, largest, smallest, M;
 
     printk(KERN_INFO "Advanced Kernel Module Initialized\n");
 
-    // Add test elements to the list
-    for (i = 0; i < 7; ++i) {
+    for (i = 0; i < 7; ++i)
+    {
         new_node = kmalloc(sizeof(*new_node), GFP_KERNEL);
         new_node->data = test_data[i];
         list_add_tail(&new_node->list, &my_list_head);
@@ -35,15 +37,18 @@ static int __init advanced_kernel_init(void) {
 
     // Task 4.1: Conjunction of All List Elements
     conjunction = 0xFF;
-    list_for_each_entry(cursor, &my_list_head, list) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
         conjunction &= cursor->data;
     }
     printk(KERN_INFO "Conjunction of all elements: %d\n", conjunction);
 
     // Task 4.2: Find the Largest Value in the List
     largest = INT_MIN;
-    list_for_each_entry(cursor, &my_list_head, list) {
-        if (cursor->data > largest) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
+        if (cursor->data > largest)
+        {
             largest = cursor->data;
         }
     }
@@ -51,8 +56,10 @@ static int __init advanced_kernel_init(void) {
 
     // Task 4.3: Find the Smallest Value in the List
     smallest = INT_MAX;
-    list_for_each_entry(cursor, &my_list_head, list) {
-        if (cursor->data < smallest) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
+        if (cursor->data < smallest)
+        {
             smallest = cursor->data;
         }
     }
@@ -60,37 +67,42 @@ static int __init advanced_kernel_init(void) {
 
     // Task 4.4: Divide List Elements by M
     M = 25;
-    list_for_each_entry(cursor, &my_list_head, list) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
         cursor->data /= M;
         printk(KERN_INFO "Element divided by %d: %d\n", M, cursor->data);
     }
 
     // Task 4.5: Shift List Elements Left by 1 Bit
-    list_for_each_entry(cursor, &my_list_head, list) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
         cursor->data <<= 1;
         printk(KERN_INFO "Element shifted left: %d\n", cursor->data);
     }
 
     // Task 4.6: Shift List Elements Right by 1 Bit
-    list_for_each_entry(cursor, &my_list_head, list) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
         cursor->data >>= 1;
         printk(KERN_INFO "Element shifted right: %d\n", cursor->data);
     }
 
     // Task 4.7: Invert List Elements
-    list_for_each_entry(cursor, &my_list_head, list) {
+    list_for_each_entry(cursor, &my_list_head, list)
+    {
         cursor->data = ~cursor->data;
         printk(KERN_INFO "Element inverted: %d\n", cursor->data);
     }
 
-    return 0; // Return 0 means module successfully loaded
+    return 0;
 }
 
-static void __exit advanced_kernel_exit(void) {
+static void __exit advanced_kernel_exit(void)
+{
     struct my_list *cursor, *tmp;
 
-    // Traverse and free the list
-    list_for_each_entry_safe(cursor, tmp, &my_list_head, list) {
+    list_for_each_entry_safe(cursor, tmp, &my_list_head, list)
+    {
         list_del(&cursor->list);
         kfree(cursor);
     }
@@ -100,4 +112,3 @@ static void __exit advanced_kernel_exit(void) {
 
 module_init(advanced_kernel_init);
 module_exit(advanced_kernel_exit);
-
--- cpp/lab/lab-4-21.11.2023/hello_colonel/hello_colonel.c
+++ cpp/lab/lab-4-21.11.2023/hello_colonel/hello_colonel.c
@@ -7,15 +7,16 @@ MODULE_AUTHOR("Mike Molchanov");
 MODULE_DESCRIPTION("A simple hello world kernel module");
 MODULE_VERSION("0.1");
 
-static int __init hello_kernel_init(void) {
+static int __init hello_kernel_init(void)
+{
    printk(KERN_INFO "Hello, Kernel!\n");
    return 0;
 }
 
-static void __exit hello_kernel_exit(void) {
+static void __exit hello_kernel_exit(void)
+{
    printk(KERN_INFO "Goodbye, Kernel!\n");
 }
 
 module_init(hello_kernel_init);
 module_exit(hello_kernel_exit);
-
