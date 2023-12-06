677446be02a83a4a9d2ea6bf632ba8e3bd6774b6
kiIIer <39669501+kiIIer@users.noreply.github.com>
2023-12-06 06:42:09 +0200

    Cpp/lab/4 (#63)
    * tasks done
    
    * formated files and prepared report

---
+++ cpp/lab/lab-4-21.11.2023/hello_colonel/hello_colonel.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mike Molchanov");
+MODULE_DESCRIPTION("A simple hello world kernel module");
+MODULE_VERSION("0.1");
+
+static int __init hello_kernel_init(void)
+{
+   printk(KERN_INFO "Hello, Kernel!\n");
+   return 0;
+}
+
+static void __exit hello_kernel_exit(void)
+{
+   printk(KERN_INFO "Goodbye, Kernel!\n");
+}
+
+module_init(hello_kernel_init);
+module_exit(hello_kernel_exit);
